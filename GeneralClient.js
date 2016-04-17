6b6ab68ac11db6c3c744d39e4ae516118ba13508
akwiat <alexk614@stanford.edu>
2016-04-17 15:07:35 -0700

    GeneralClient

--- GeneralClient.js
+++ GeneralClient.js
@@ -1,3 +1,16 @@
-function GeneralClient() {
-
+function GeneralClient(loadGraphicsFn, initObj) {
+  this.loadGraphicsFn = loadGraphicsFn;
+  this.initObj = initObj;
+  
+  if (this.loadGraphicsFn)
+    this.loadGraphcisFn(this.onLoad.bind(this));
+  //this.loadGraphics();
+  //if (GeneralGraphics.loadEverything)
+	  //GeneralGraphics.loadEverything(this.onLoad.bind(this));
 }
+
+GeneralClient.prototype.onLoad = function() {
+	this.gameStructure = new InitializeClientStructure(this.initObj);
+}
+
+
