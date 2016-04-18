7313c3b9bd2aabded284ecd4813c2aff9ddcafef
Catherine Dong <cdong@cs.stanford.edu>
2016-04-17 22:46:26 -0700

    fixed spelling errors lol

--- ShotManager.js
+++ ShotManager.js
@@ -2,59 +2,55 @@ var INITIAL_SHOTS = 2;
 var PLAYERS_PER_TEAM = 2;
 
 function ShotManager(playerId, teammates, onShoot, onDeactivate) {
-	this.playerId = playerId; 				// key in global player array
-	this.teammates = teammates; 			// array of teammates' IDs
-	this.onShoot = onShoot;
-	this.shotsRemaining = INITIAL_SHOTS;	// number of shots remaining for team
-	this.shieldUp = false;					// defense shield status
-	this.playerSheilds = []				// defense sheild status of all players
-	for (var i = 0; i < PLAYERS_PER_TEAM * 2; i++) {
-		this.playerSheilds.push(false);
-	}
+  this.playerId = playerId;         		// key in global player array
+  this.teammates = teammates;       		// array of teammates' IDs
+  this.onShoot = onShoot;								// callback to invoke on shooting
+  this.shotsRemaining = INITIAL_SHOTS;  // number of shots remaining for team
+  this.shieldUp = false;          			// defense shield status
+  this.playerShields = []        				// defense shield status of all players
+  for (var i = 0; i < PLAYERS_PER_TEAM * 2; i++) {
+    this.playerShields.push(false);
+  }
 }
 
 ShotManager.prototype.shoot = function() {
-	if (this.shotsRemaining > 0) {
-		this.shotsRemaining--;
-		this.activateShield();
-		// TODO: start shield timer
-		// TODO: call onShoot
-		return true;
-	} else {
-		return false;
-	}
+  if (this.shotsRemaining > 0) {
+    this.shotsRemaining--;
+    this.activateShield();
+    // TODO: start shield timer
+    // TODO: call onShoot
+    return true;
+  } else {
+    return false;
+  }
 }
 
-// fn to update status if some other player has shot
-ShotManager.prototype.shotOccured = function(shooter) {
-	this.playerSheilds[shooter] = true;
+ShotManager.prototype.shotOccurred = function(shooter) {
+  this.playerShields[shooter] = true;
 
-	for (var i = 0; i < this.teammates.length; i++) {
-		if (this.teammates[i] == shooter) {
-			this.shotsRemaining--;
-			return;
-		}
-	}
+  for (var i = 0; i < this.teammates.length; i++) {
+    if (this.teammates[i] == shooter) {
+      this.shotsRemaining--;
+      return;
+    }
+  }
 }
 
-// fn to put up shield (called by server when shot is sent)
 ShotManager.prototype.activateShield = function() {
-	this.shieldUp = true;
+  this.shieldUp = true;
 }
 
-// fn to take down shield (called by server shield expires)
 ShotManager.prototype.deactivateShield = function() {
-	this.shield = false;
+  this.shield = false;
 }
 
 
-// fm that says whos sheilds are up
 ShotManager.prototype.currentShooters = function() {
-	shooters = []
-	for (var i = 0; i < this.playerSheilds; i++) {
-		if (this.playerSheilds[i] = true) {
-			shooters.push(i)
-		}
-	}
-	return shooters;
+  shooters = []
+  for (var i = 0; i < this.playerShields; i++) {
+    if (this.playerShields[i] = true) {
+      shooters.push(i)
+    }
+  }
+  return shooters;
 }
