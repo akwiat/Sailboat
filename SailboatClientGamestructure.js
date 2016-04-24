528018edc5b474a35c70de8f1399dbf989f9a233
Alex Kwiatkowski <alexk614@stanford.edu>
2016-04-24 14:02:15 -0700

    shoot with p

--- SailboatClientGamestructure.js
+++ SailboatClientGamestructure.js
@@ -36,7 +36,7 @@ Sailboat.Client.prototype.gameStructureHasInitialized = function() {
 			this["gameStructure"]["client"].controlsManager.addControl(this.shipControl);
 
 			this["gameStructure"]["client"].controlsManager.addControl(
-				new ThreexControlsAction(this.onShoot.bind(this), "f")
+				new ThreexControlsAction(this.onShoot.bind(this), "p")
 				);
 
 
