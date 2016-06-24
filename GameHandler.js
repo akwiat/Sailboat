c4bc23eb28b40066767537ccf0b82223d416582e
akwiat <alexk614@stanford.edu>
2016-06-24 10:22:02 -0700

    Update GameHandler.js
    GameHandler clean

--- GameHandler.js
+++ GameHandler.js
@@ -7,33 +7,14 @@ var GameStateEntity = require("./GameStateEntity").GameStateEntity;
 
 
 
-//function GameHandler(link, gs) {
 function GameHandler(GameStateType) {
-	//this.gs = gs;
-	//this.serverLink = link;
-
-	/*
-	this.handlerBridge
-	implicit from structure
-	*/
 
 	this.gs = new GameStateType();
 	this.playerList = [];
 	this.st = undefined;
-	/*
-	this.physicsLoopInterval = 2000;
-	this.updateLoopInterval = 2000;
-
-	this.st = new Date().getTime();//autostart
-
-	this.physicsLoopId = setInterval(this.physicsLoop.bind(this,this.st), this.physicsLoopInterval);
-	this.updateLoopId = setInterval(this.updateLoop.bind(this,this.st),this.updateLoopInterval);
-
-	*/
-	//--this.sendstate = new DifferenceObj(this.gs.fragMaker.bind(this.gs))
 	this.sendstate = new DifferenceObj();
 	this.init();
-	//this.loopnum
+
 
 }
 GameHandler.prototype.getObjByName = function(name){
@@ -48,15 +29,6 @@ GameHandler.prototype.getFullStateDifObj = function() {
 GameHandler.prototype.getGameTime = function() {
 	return new Date().getTime()/1000 - this.st;
 }
-/*
-GameHandler.prototype.physicsLoop = function(st) {
-	var ct = new Date().getTime() - st;
-}
-GameHandler.prototype.updateLoop = function(st) {
-  	var ct = new Date().getTime() - st;
-  	util.log("parent updateLoop");
-}
-*/
 GameHandler.prototype.init = function() {
 	//throw new Error("test");
 	this.st = new Date().getTime()/1000;
@@ -100,18 +72,6 @@ GameHandler.prototype.unOfficialNewObj = function(obj) {
 	var index = this.gs.addNew(obj);
 	return obj;
 }
-
-/*
-GameHandler.prototype.officialRemove = function(gc, oi) {
-	var f = Frag.removalFrag(gc, oi);
-	this.officialChange(f);
-}
-*/
-/*
-GameHandler.prototype.connectionReady = function(id) {
-	//can initialize the new player
-}
-*/
 GameHandler.prototype.receiveUpdate = function(obj) {
 	/*
 	if (console)
@@ -121,9 +81,9 @@ GameHandler.prototype.receiveUpdate = function(obj) {
 */
 	var difo = DifferenceObj.makeFromObj(obj);
 	this.gs.applyDifObj(difo);
-	//this.sendstate.appendDifObj(difo);
+
 	return difo;
-	//this.sendstate.appendDifObj(difo);
+	
 }
 GameHandler.prototype.receiveAndPassUpdate = function(obj) {
 	var difo = this.receiveUpdate(obj);
@@ -134,57 +94,6 @@ GameHandler.prototype.pullSendstate = function() {
 	return ret;
 	//this.send
 }
-/*
-GameHandler.prototype.sendState = function(sendable) {
-
-}
-*/
-/*
-GameHandler.prototype.update = function(obj) {
-	throw new Error("deprecated")
-	var difo = DifferenceObj.makeFromObj(obj);
-	this.gs.apply(difo,this.sendstate);
-}*/
-/*
-GameHandler.prototype.newPlayer = function() {
-	return this.gs.addNewPlayer();
-}
-GameHandler.prototype.onNewConnection = function() {
-	util.log("subclass should override onNewConnection");
-	return 12;
-}
-GameHandler.prototype.clientDisconnected = function(clientId) {
-  util.log("clientDisconnected: "+clientId);
-}
-*/
-/*
-GameHandler.replacer = function(k,v) {
-	if (v===undefined) return undefined;
-	//if (k=="updatetime") return undefined;
-	//GameHandler.replacer.pgsid = undefined;
-	if (v.t === Player.TID
-		&&v.i === GameHandler.replacer.pgsid)
-	{
-		var ret = {};
-		for (var i in v) {
-			if (v.hasOwnProperty(i)) {
-				ret[i] = v[i];
-			}
-		}
-		ret.p = undefined;
-		ret.q = undefined;
-		ret.v = undefined;
-		//ret.l = undefined;
-		return ret;
-	}
-
-	return v;
-}
-*/
-/*
-GameHandler.prototype.message = function(str) {
-}
-*/
 
 if (!this.___alexnorequire) 
 exports.GameHandler = GameHandler;
