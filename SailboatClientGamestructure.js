7783b64c4426e0c891f8064fab2d44d2c6bde937
akwiat <alexk614@stanford.edu>
2016-06-30 22:40:41 -0700

    customMessageManager

--- SailboatClientGamestructure.js
+++ SailboatClientGamestructure.js
@@ -2,6 +2,7 @@ Sailboat.Client.prototype.gameStructureHasInitialized = function() {
 	
 	
 	var gameStructureCallbacks = this.gameStructure.callbacks;
+	/*
 	var updateLoop = function() {
 		if (this["gameHandler"].myPlayer) {
 		var frag = this["gameHandler"].myPlayer.getSpecificFrag();
@@ -11,6 +12,43 @@ Sailboat.Client.prototype.gameStructureHasInitialized = function() {
 		this["handlerBridge"].sendUpdateToServer();
 		}
 	}
+	*/
+	var customMessageId = function(msg) {
+		this.setMyId(msg);
+	}
+	this.customMessageManager.subscribeToMessage("i", customMessageId);
+	var customMessageInitP = function(str) {
+		var state = this["gameStructure"]["gameHandler"].gs;
+			var frag = Frag.makeFromStr(str);
+			console.log(frag);
+			this["gameStructure"]["gameHandler"].setCurrentGameTime(frag.updateTime);
+			state.applyFrag(frag);
+			//this["gameStructure"]["gameHandler"].myPlayer = state.entity.children[0].children[frag.specificData];
+			this["gameStructure"]["gameHandler"].myPlayer = state.entity.getObjFromPath(frag.specificData);
+			var myPlayer = this["gameStructure"]["gameHandler"].myPlayer;
+			var arrayName = myPlayer.parent.getIdentifier();
+			//debugger;
+			this["gameStructure"]["client"].setupTeamFunctions(arrayName);
+
+			//var myShip = myPlayer.findChildWithIdentifier("ship")
+			var myShip = myPlayer.getChildByIdentifier("shipArray").children[0];
+			var myShipPos = myShip.findChildWithIdentifier("position").getWrappedObj();
+
+			//this.shipControl = new ThreexControlsCircleMover(myShipPos, "w", "s", "d", "a");
+			this.shipControl = new ThreexControlsCircleMover(myShipPos, "w", "s", "d", "a");
+			this.teamSpecificControls(this.shipControl);
+			
+			this["gameStructure"]["client"].controlsManager.addControl(this.shipControl);
+
+			this["gameStructure"]["client"].controlsManager.addControl(
+				new ThreexControlsAction(this.onShoot.bind(this), " ")
+				);
+
+
+			//debugger;
+	}
+	this.customMessageManager.subscribeToMessage("p", customMessageInitP);
+	/*
 	var clientCustomMsg = function(msg) {
 		//console.log("client custom msg: "+msg);
 		var c = msg.charAt(0);
@@ -55,8 +93,8 @@ Sailboat.Client.prototype.gameStructureHasInitialized = function() {
 		}
 	}
 	gameStructureCallbacks.register(clientCustomMsg.bind(this), GameStructureCodes.CLIENTGOTCUSTOMMSG);
-
-	this.updateLoopId = setInterval(updateLoop.bind(this.gameStructure), 50);
+	*/
+	//this.updateLoopId = setInterval(updateLoop.bind(this.gameStructure), 50);
 	this.graphics = new SailboatGraphics(this.graphicsSettings);
 	this.controlsManager = new ThreexControlsManager(Crafty);
 	
@@ -64,7 +102,7 @@ Sailboat.Client.prototype.gameStructureHasInitialized = function() {
 	this.cooldownManager = new CooldownManager();
 
 
-	//this.respawnCooldown = new 
+	
 	this.graphics.callbacks.register(this.onFrame.bind(this), "OnFrame");
 	this.graphics.callbacks.register(this.onDeadShip.bind(this), "OnDeadShip");
 	this.registerGameStateCallbacks(); 
