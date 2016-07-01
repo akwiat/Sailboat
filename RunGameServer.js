39d9bd350a85e29bf922654c9454151f457cd1d1
akwiat <alexk614@stanford.edu>
2016-07-01 11:31:20 -0700

    Update RunGameServer.js

--- RunGameServer.js
+++ RunGameServer.js
@@ -1,5 +1,5 @@
 var InitializeServerStructure = require("./GameStructure").InitializeServerStructure;
 var Sailboat = require("./Sailboat").Sailboat;
 var gameStructure = new InitializeServerStructure(Sailboat.getInitObj());
-var sailboatServer = new Sailboat.Server(gameStructure);
+Sailboat.Server(gameStructure); //now Sailboat.Server is just a function that makes the necessary customizations
 
