9e9a49ad3e92a1c3de6add1e64d320e2182bcb84
akwiat <alexk614@stanford.edu>
2016-07-05 21:46:43 -0700

    just need the gamestructure

--- RunGameServer.js
+++ RunGameServer.js
@@ -1,5 +1,5 @@
 var InitializeServerStructure = require("./GameStructure").InitializeServerStructure;
 var Sailboat = require("./Sailboat").Sailboat;
 var gameStructure = new InitializeServerStructure(Sailboat.getInitObj());
-Sailboat.Server(gameStructure); //now Sailboat.Server is just a function that makes the necessary customizations
+//Sailboat.Server(gameStructure); //now Sailboat.Server is just a function that makes the necessary customizations
 
