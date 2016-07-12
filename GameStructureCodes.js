68e497bf9cdb5cad505b20fc939e20d3138b69c8
akwiat <alexk614@stanford.edu>
2016-07-12 12:28:21 -0700

    unnecessary codes

--- GameStructureCodes.js
+++ GameStructureCodes.js
@@ -1,9 +1,11 @@
 function GameStructureCodes() {}
+/*
 GameStructureCodes.SERVERGOTCUSTOMMSG = "servergotcustommsg";
 GameStructureCodes.SERVERNEWCONN = "servernewconn";
 GameStructureCodes.SERVERCLIENTDISC = "serverclientdisc";
 GameStructureCodes.SERVERINITPLAYER = "serverinitplayer";
 GameStructureCodes.CLIENTGOTCUSTOMMSG = "clientgotcustommsg";
+*/
 GameStructureCodes.INFORMCLIENTID = "informclientid";
 
 if (!this.___alexnorequire) {
