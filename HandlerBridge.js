35d80bd72f6f80226f4c897220b316737f87bcb3
akwiat <alexk614@stanford.edu>
2016-07-12 12:35:44 -0700

    informclientid callback

--- HandlerBridge.js
+++ HandlerBridge.js
@@ -103,6 +103,7 @@ HandlerBridgeClientSide.prototype.subscribeToInitPackage = function(fn) {
 HandlerBridgeClientSide.prototype.informClientId = function(id) {
 	//throw new Error("should reach");
 	this["clientBehavior"].setMyId(id);
+	this.gameStructure.trigger(undefined, GameStructureCodes.INFORMCLIENTID);
 }
 HandlerBridgeClientSide.prototype.receiveMsg = function(msg) {
 	var c = msg.charAt(0);
