1342c2b1ecb0b139f7f2b98ef5510f51746a6b9d
akwiat <alexk614@stanford.edu>
2016-07-12 11:17:49 -0700

    beginLoading

--- reusable/LoadingManager.js
+++ reusable/LoadingManager.js
@@ -1,12 +1,20 @@
 define( function() {
     function LoadingManager(strArray, completeFn) { //is LoadingManager
       this.requirements = {};
+      this.loadFns = [];
       if(strArray) for (var o of strArray) this.registerRequirement(o);
       this.completeFunction = completeFn;
     }
-    LoadingManager.prototype.registerRequirement(str) {
+    LoadingManager.prototype.beginLoading = function() {
+        for (var i=0; i < this.loadFns.length; i++)
+          this.loadFns[i]();
+    }
+    LoadingManager.prototype.registerRequirement(str, fn) {
       if (this.requirements[str] !== undefined) throw new Error("bad requirement: "+str);
       this.requirements[str] = false;
+      
+      var onComplete = this.completeRequirement.bind(this, str);
+      this.loadFns.push(fn.bind(null, onComplete)); //fn accepts the callback as its argument. this stores that to be called on beginLoading
     }
     LoadingManager.prototype.completeRequirement(str) {
       if (this.requirements[str] !== false) throw new Error("bad completed requirement: "+str);
